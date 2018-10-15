d7a2ec662d13c286597c9a7b6c2ec4528a0d1887
Sigrid Keydana <keydana@gmx.de>
2018-10-15 21:45:38 +0200

    wip

--- mmdvae/mmd_cvae.R
+++ mmdvae/mmd_cvae.R
@@ -1,32 +1,42 @@
 # https://github.com/tensorflow/tensorflow/blob/r1.11/tensorflow/contrib/eager/python/examples/generative_examples/cvae.ipynb
 
+reticulate::use_condaenv("tf-12-gpu-0410")
+
 library(keras)
 use_implementation("tensorflow")
 library(tensorflow)
 tfe_enable_eager_execution(device_policy = "silent")
 
 library(tfdatasets)
+library(dplyr)
+library(ggplot2)
+library(glue)
 
-images %<-% tf$keras$datasets$mnist$load_data()[[1]][[1]]
-images %>% dim()
+mnist <- dataset_mnist()
 
-images[images[, , ] < 127] <- 0
-images[images[, , ] >= 127] <- 1
+c(train_images, train_labels) %<-% mnist$train
+c(test_images, test_labels) %<-% mnist$test
 
+#images[images[, , ] < 127] <- 0
+#images[images[, , ] >= 127] <- 1
 
-images <- images %>% k_reshape(c(60000, 28, 28, 1))
+train_x <- train_images %>% `/`(255) %>% k_reshape(c(60000, 28, 28, 1))
+test_x <- test_images %>% `/`(255) %>% k_reshape(c(10000, 28, 28, 1))
 
-buffer_size <- 1000
+buffer_size <- 60000
 batch_size <- 100
+batches_per_epoch <- buffer_size/batch_size
 
-dataset <- tensor_slices_dataset(images) %>%
+train_dataset <- tensor_slices_dataset(train_x) %>%
   dataset_shuffle(buffer_size) %>%
   dataset_batch(batch_size)
 
+test_dataset <- tensor_slices_dataset(test_x) %>%
+  dataset_batch(10000)
 
-latent_dim <- 50
+latent_dim <- 2
 
-encoder <- function(name = NULL) {
+encoder_model <- function(name = NULL) {
   keras_model_custom(name = name, function(self) {
     self$conv1 <-
       layer_conv_2d(
@@ -35,7 +45,7 @@ encoder <- function(name = NULL) {
         strides = 2,
         activation = "relu"
       )
-    self$conv1 <-
+    self$conv2 <-
       layer_conv_2d(
         filters = 64,
         kernel_size = 3,
@@ -51,12 +61,12 @@ encoder <- function(name = NULL) {
         self$conv2() %>%
         self$flatten() %>% 
         self$dense() %>%
-        tf$split(num_or_size_splits = 2, axis = 1)
+        tf$split(num_or_size_splits = 2L, axis = 1) # 0-based
     }
   })
 }
 
-decoder <- function(name = NULL) {
+decoder_model <- function(name = NULL) {
   keras_model_custom(name = name, function(self) {
     self$dense <- layer_dense(units = 7 * 7 * 32, activation = "relu")
     self$reshape <- layer_reshape(target_shape = c(7, 7, 32))
@@ -70,7 +80,7 @@ decoder <- function(name = NULL) {
         activation = "relu"
       )
     self$deconv2 <-
-      layer_conv_2d(
+      layer_conv_2d_transpose(
         filters = 32,
         kernel_size = 3,
         strides = 2,
@@ -84,7 +94,6 @@ decoder <- function(name = NULL) {
         strides = 1,
         padding = "same"
       )
-
     
     function (x, mask = NULL) {
       x %>% 
@@ -93,8 +102,6 @@ decoder <- function(name = NULL) {
         self$deconv1() %>% 
         self$deconv2() %>% 
         self$deconv3()
-      
-      ##if apply_sigmoid:       probs = tf.sigmoid(logits)
     }
   })
 }
@@ -107,14 +114,13 @@ reparameterize <- function(mean, logvar) {
   tf$exp(logvar * .5) + mean
 }
 
-# In practice, we optimize the single sample Monte Carlo estimate of this expectation:
-# log p(x| z) + log p(z) - log q(z|x)
-# where z is sampled from q(z|x).
-log_normal_pdf <- function(sample, mean, logvar, reduce_axis = 1) {
-  log2pi = tf$log(2 * np$pi)
-  tf$reduce_sum(
-    -.5 * ((sample - mean)^2 * tf$exp(-logvar) + logvar + log2pi),
-    axis = reduce_axis)
+
+normal_loglik <- function(sample, mean, logvar, reduce_axis = 1L) {
+  loglik <-  k_constant(0.5, dtype = tf$float64) * (
+    tf$log(2 * k_constant(pi, dtype = tf$float64)) + 
+    logvar + 
+    tf$exp(-logvar) * (sample - mean)^2)
+  - tf$reduce_sum(loglik, axis = reduce_axis)
 }
  
 optimizer <- tf$train$AdamOptimizer(1e-4)
@@ -122,55 +128,145 @@ optimizer <- tf$train$AdamOptimizer(1e-4)
 
 # Generation --------------------------------------------------------------
 
-random_vector_for_generation <- tf$random_normal(
-  shape = list(num_examples_to_generate, latent_dim))
+num_examples_to_generate <- 16
 
-generate_and_save_images <- function(
+random_vector_for_generation <- k_random_normal(
+  shape = list(num_examples_to_generate, latent_dim),
+  dtype = tf$float64)
+
+generate_random_digits <- function(
   epoch) {
-  predictions <-decoder(random_vector_for_generation) %>% tf$nn$sigmoid()
-  # tbd plot
+  predictions <- decoder(random_vector_for_generation) %>% tf$nn$sigmoid()
+  png(paste0("digits_epoch_", epoch, ".png"))
+  par(mfcol = c(4, 4))
+  par(mar = c(0.5, 0.5, 0.5, 0.5),
+      xaxs = 'i',
+      yaxs = 'i')
+  for (i in 1:16) {
+    img <- predictions[i, , , 1]
+    img <- t(apply(img, 2, rev))
+    image(
+      1:28,
+      1:28,
+      img * 127.5 + 127.5,
+      col = gray((0:255) / 255),
+      xaxt = 'n',
+      yaxt = 'n'
+    )
+  }
+  dev.off()
 }
 
+
+show_latent_space <- function(epoch) {
+  
+  iter <- make_iterator_one_shot(test_dataset)
+  x <-  iterator_get_next(iter)
+  x_test_encoded <- encoder(x)[[1]]
+  x_test_encoded %>%
+    as.matrix() %>%
+    as.data.frame() %>%
+    mutate(class = as.factor(mnist$test$y)) %>%
+    ggplot(aes(x = V1, y = V2, colour = class)) + geom_point() +
+    theme(aspect.ratio = 1)
+  ggsave(paste0("latentspace_epoch_", epoch, ".png"), width = 10, height = 10, units = "cm")
+}
+
+
+show_grid <- function(epoch) {
+  n <- 16  
+  digit_size <- 28
+  
+  grid_x <- seq(-4, 4, length.out = n)
+  grid_y <- seq(-4, 4, length.out = n)
+  
+  rows <- NULL
+  for(i in 1:length(grid_x)){
+    column <- NULL
+    for(j in 1:length(grid_y)){
+      z_sample <- matrix(c(grid_x[i], grid_y[j]), ncol = 2)
+      column <- rbind(column, (decoder(z_sample) %>% tf$nn$sigmoid() %>% as.numeric()) %>% matrix(ncol = digit_size))
+    }
+    rows <- cbind(rows, column)
+  }
+  rows %>% as.raster() %>% plot()
+}
 
 
 # Training loop -----------------------------------------------------------
 
 num_epochs <- 10#0
 
+encoder <- encoder_model()
+decoder <- decoder_model()
 
 for (epoch in seq_len(num_epochs)) {
-  iter <- make_iterator_one_shot(dataset)
+  iter <- make_iterator_one_shot(train_dataset)
+  
   total_loss <- 0
+  logpx_z_total <- 0
+  logpz_total <- 0
+  logqz_x_total <- 0
   
   until_out_of_range({
     x <-  iterator_get_next(iter)
     
-    with(tf$GradientTape() %as% tape, {
+    with(tf$GradientTape(persistent = TRUE) %as% tape, {
       
+      # both of shape (bs, 50)
       c(mean, logvar) %<-% encoder(x)
       z <- reparameterize(mean, logvar)
+      # shape is (bs, 28, 28, 1)
       preds <- decoder(z)
+      
+      # ELBO
+      # log p(x| z) + log p(z) - log q(z|x)
+      # way 1: all three terms estimated by Monte Carlo 
       crossentropy_loss <-
         tf$nn$sigmoid_cross_entropy_with_logits(logits = preds, labels = x)
-      logpx_z <- -tf$reduce_sum(crossentropy_loss, axis = list(1, 2, 3))
-      logpz <- log_normal_pdf(z, 0, 0)
-      logqz_x <- log_normal_pdf(z, mean, logvar)
+      logpx_z <- -tf$reduce_sum(crossentropy_loss, axis = list(1, 2, 3)) 
+      logpz <- normal_loglik(z, k_constant(0, dtype = tf$float64), k_constant(0, dtype = tf$float64))
+      logqz_x <- normal_loglik(z, mean, logvar)
       loss <- -tf$reduce_mean(logpx_z + logpz - logqz_x)
+      
+      
       
     })
     
+    # cat(
+    #   glue(
+    #     "Losses (epoch): {epoch}:",
+    #     "  {as.numeric(logpx_z %>% tf$reduce_mean()) %>% round(2)} logpx_z,",
+    #     "  {as.numeric(logpz  %>% tf$reduce_mean()) %>% round(2)} logpz,",
+    #     "  {as.numeric(logqz_x  %>% tf$reduce_mean()) %>% round(2)} logqz_x,",
+    #     "  {as.numeric(loss) %>% round(2)} loss"),
+    #   "\n")
+    
     total_loss <- total_loss + loss
+    logpx_z_total <- tf$reduce_mean(logpx_z) + logpx_z_total
+    logpz_total <- tf$reduce_mean(logpz) + logpz_total
+    logqz_x_total <- tf$reduce_mean(logqz_x) + logqz_x_total
 
-    gradients <- tape$gradient(loss, model$variables)
-    optimizer$apply_gradients(purrr::transpose(list(gradients, model$variables)),
+    encoder_gradients <- tape$gradient(loss, encoder$variables)
+    decoder_gradients <- tape$gradient(loss, decoder$variables)
+    optimizer$apply_gradients(purrr::transpose(list(encoder_gradients, encoder$variables)),
+                              global_step = tf$train$get_or_create_global_step())
+    optimizer$apply_gradients(purrr::transpose(list(decoder_gradients, decoder$variables)),
                               global_step = tf$train$get_or_create_global_step())
     
   })
   
-  cat("Total loss (epoch): ", i, ": ", as.numeric(total_loss), "\n")
+  cat(
+    glue(
+    "Losses (epoch): {epoch}:",
+    "  {(as.numeric(logpx_z_total)/batches_per_epoch) %>% round(2)} logpx_z_total,",
+    "  {(as.numeric(logpz_total)/batches_per_epoch) %>% round(2)} logpz_total,",
+    "  {(as.numeric(logqz_x_total)/batches_per_epoch) %>% round(2)} logqz_x_total,",
+    "  {(as.numeric(total_loss)/batches_per_epoch) %>% round(2)} total"),
+    "\n")
   
   if (epoch %% 1 == 0) {
-    generate_and_save_images(
-      epoch, random_vector_for_generation)
+    generate_random_digits(
+      epoch)
   }
 }
